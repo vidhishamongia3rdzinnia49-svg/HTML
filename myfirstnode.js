0d285c294f64ed959c05ae4b68339febdbfd39da
vidhishamongia3rdzinnia49-svg <vidhishamongia3rdzinnia49@gmail.com>
2026-01-01 18:59:46 +0530

    Add files via upload

---
+++ myfirstnode.js
@@ -0,0 +1,27 @@
+console.log("MY NAME IS VIDHISHA")
+var a=8;
+var b=10;
+var c=10;
+var d=null;
+var e;
+console.log("a and b are equal:"+(a==b));
+console.log("\n");
+console.log("b and c are equal:"+(b==c));
+console.log("\n");
+console.log("value  of d:"+d);
+console.log("\n");
+console.log("value  of e:"+e);
+var day=new Date();
+
+console.log(day.getDate()+":"+"1"+":"+day.getFullYear())
+var greet=new Date();
+hour=greet.getHours()
+if (hour<12){
+    console.log("GOOD MORNING");
+}
+else if (hour<17){
+    console.log("GOOD AFTERNOON");
+}
+else if (hour<21){
+    console.log("GOOD EVENING");
+}
